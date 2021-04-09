1233ee51a7e5c90848722d6b624b4a3196122593
Rayna W <raynagweinreb@gmail.com>
2021-04-08 18:47:03 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+//dependacies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db")
+
+//express 
+var app = express();
+var PORT = process.env.PORT|| 8080;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//set on page load 
+app.get("/", function (req,res){
+    res.sendFile(path.joing(__dirname, "public/index.html"));
+})
+
+app.get("/notes", function (req,res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+
+// API get 
+app.route("/api/notes").get(function(req,res){
+    res.json(database)
+})
+
+//add new note to db 
+.post(function(req,res){
+    let jsonDB = path.jpin(__dirname, "/db/db.json");
+    let newNote= req.body;
+
+fs.writeFile(jsonDB, JSON.stringifu(database),function(err){
+    if (err) {
+        return console.log(err)
+    }
+    console.log("Congratulations! Your note was saved")
+})
+res.json(newNote)
+})
+app.listen(port,function(){
+    console.log("app listening on PORT"+ PORT )
+})
